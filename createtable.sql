022e803c8d80f20f96df7b3c3e1b2671e7aeb800
formidify <james.yangjinhui@gmail.com>
2017-10-06 13:52:07 -0500

    changed data file

--- createtable.sql
+++ createtable.sql
@@ -1,12 +1,11 @@
-DROP TABLE IF EXISTS earthquakes;
-CREATE TABLE earthquakes (
-  quakedate date,
-  quaketime time with time zone,
-  latitude real,
-  longitude real,
-  quakedepth real,
-  mag real,
-  id text,
-  place text,
-  quaketype text
+DROP TABLE IF EXISTS games;
+CREATE TABLE games (
+  gamename varchar(255),
+  averageRating real,
+  totalRatings real,
+  installs varchar(255) 
+  price varchar(255),
+  gameType varchar(255),
+  ios boolean,
+  availability varchar(255)
 );
