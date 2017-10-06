42954bb93ab3c2d2c824ea786733a996217c7006
formidify <james.yangjinhui@gmail.com>
2017-10-06 14:10:28 -0500

    Added data file and modified createtable.sql

--- createtable.sql
+++ createtable.sql
@@ -3,7 +3,7 @@ CREATE TABLE games (
   gamename varchar(255),
   averageRating real,
   totalRatings real,
-  installs varchar(255) 
+  installs varchar(255), 
   price varchar(255),
   gameType varchar(255),
   ios boolean,
