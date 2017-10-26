910ddb087064862905ed619e46ec4fb179056f47
formidify <james.yangjinhui@gmail.com>
2017-10-25 21:28:01 -0500

    If not final then almost

--- datasource.py
+++ datasource.py
@@ -1,7 +1,7 @@
 '''
     datasource.py
     James Yang, Vermilion Villarreal
-    Modified October 17, 2017
+    Modified October 25, 2017
 '''
 
 import getpass
@@ -37,16 +37,17 @@ class DataSource:
 	Used to provide a list of search results for the search button on our webpage. 
 	'''
 	def mainSearch(self, genre, minPlayers, searchInput):
-		minPlayers = int(minPlayers)
 		if (genre == -1 and minPlayers != -1):
-			self.cursor.execute('SELECT * FROM boardgames WHERE minPlayer >= %s', (minPlayers,))
+			self.cursor.execute('SELECT gameName, thumbnailURL, gameCategory, gameMechanic, gameID \
+			FROM boardgames WHERE minPlayer >= %s', (minPlayers,))
 			if self.cursor:
 				resultList = self.cursor.fetchall()
 			else:
 				return -1
 
 		if (genre != -1 and minPlayers == -1):
-			self.cursor.execute('SELECT * FROM boardgames')
+			self.cursor.execute('SELECT gameName, thumbnailURL, gameCategory, gameMechanic, gameID \
+			FROM boardgames')
 			if self.cursor:
 				resultList = self.cursor.fetchall()
 				resultList = self.findGenres(resultList, genre)
@@ -54,14 +55,16 @@ class DataSource:
 				return -1
 
 		if (genre == -1 and minPlayers == -1):
-			self.cursor.execute('SELECT * FROM boardgames')
+			self.cursor.execute('SELECT gameName, thumbnailURL, gameCategory, gameMechanic, gameID \
+			FROM boardgames')
 			if self.cursor:
 				resultList = self.cursor.fetchall()
 			else:
 				return -1
 
 		if (genre != -1 and minPlayers != -1):
-			self.cursor.execute('SELECT * FROM boardgames WHERE minPlayer >= %s', (minPlayers,))
+			self.cursor.execute('SELECT gameName, thumbnailURL, gameCategory, gameMechanic, gameID \
+			FROM boardgames WHERE minPlayer >= %s', (minPlayers,))
 			if self.cursor:
 				resultList = self.cursor.fetchall()
 				resultList = self.findGenres(resultList, genre)
@@ -71,19 +74,40 @@ class DataSource:
 		if resultList != -1:
 			if searchInput:
 				return self.findGames(resultList, searchInput)
-			return resultList
+			resultDict = self.convertDictFromAll(resultList)
+			return resultDict
 		return -1
+
+	#helper function to convert the resulting lists from mainSearch and findGames to a dictionary with appropriate keys
+	def convertDictFromAll(self, list):
+		resultDict = []
+		for i in range(len(list)):
+			resultDict.append({"name": list[i][0], "thumbURL": list[i][1], "ID": list[i][4]})
+		return resultDict
 
 	'''
 	Helper function to generate a list where the genre of the game matches the input
 	'''
 	def findGenres(self, list, genreInput):
 		j = 0
-		resultList = []
+		bothEnd = []
+		frontEnd = []
+		backEnd = []
+		neitherEnd = []
 		for i in range(len(list)):
-			if genreInput in list[i][11] or genreInput in list[i][15]:
-				resultList.append(list[i])
+			if (" " + genreInput + " ") in list[i][2] or (" " + genreInput + " ") in list[i][3]:
+				bothEnd.append(list[i])
 				j = j + 1
+			elif (" " + genreInput) in list[i][2] or (" " + genreInput) in list[i][3]:
+				frontEnd.append(list[i])
+				j = j + 1
+			elif (genreInput + " ") in list[i][2] or (genreInput + " ") in list[i][3]:
+				backEnd.append(list[i])
+				j = j + 1
+			elif genreInput in list[i][2] or genreInput in list[i][3]:
+				neitherEnd.append(list[i])
+				j = j + 1
+		resultList = bothEnd + frontEnd + backEnd + neitherEnd
 		if resultList:
 			return resultList
 		return -1
@@ -98,19 +122,20 @@ class DataSource:
 		backEnd = []
 		neitherEnd = []
 		for i in range(len(list)):
-			if (" " + searchInput + " ") in list[i][7]:
+			if (" " + searchInput + " ") in list[i][0]:
 				bothEnd.append(list[i])
 				j = j + 1
-			elif (" " + searchInput) in list[i][7]:
+			elif (" " + searchInput) in list[i][0]:
 				frontEnd.append(list[i])
-			elif (searchInput + " ") in list[i][7]:
+			elif (searchInput + " ") in list[i][0]:
 				backEnd.append(list[i])
-			elif searchInput in list[i][7]:
+			elif searchInput in list[i][0]:
 				neitherEnd.append(list[i])
 			j = j + 1
 		resultList = bothEnd + frontEnd + backEnd + neitherEnd
 		if resultList:
-			return resultList
+			resultDict = self.convertDictFromAll(resultList)
+			return resultDict
 		return -1
 
 	'''
@@ -119,12 +144,12 @@ class DataSource:
 	'''
 	def listAZSearch(self):
 		try:
-			self.cursor.execute('SELECT gameDescription, gameName, thumbnailURL, gameID \
+			self.cursor.execute('SELECT gameName, thumbnailURL, gameID \
 			FROM boardgames ORDER BY gameName ASC')
 			if self.cursor:
 				resultList = self.cursor.fetchall()
-				resultDict = self.convertDict(resultList)
-				return resultDict
+				resultDict = self.convertDictFromSome(resultList)
+				return resultDict[0:98] #only display a subset so it won't take too long to load
 			else:
 				return -1
 		except Exception as e:
@@ -143,12 +168,12 @@ class DataSource:
 	'''
 	def newGamesSearch(self):
 		try:
-			self.cursor.execute('SELECT gameDescription, gameName, thumbnailURL, gameID FROM boardgames \
+			self.cursor.execute('SELECT gameName, thumbnailURL, gameID FROM boardgames \
 			where yearPublished>2000 ORDER BY yearPublished DESC')
 			if self.cursor:
 				resultList = self.cursor.fetchall()
-				resultDict = self.convertDict(resultList)
-				return resultDict
+				resultDict = self.convertDictFromSome(resultList)
+				return resultDict[0:98]
 			else:
 				return -1
 		except Exception as e:
@@ -170,10 +195,10 @@ class DataSource:
 		try:
 			for i in range(6):
 				r = random.randint(1, 90400)
-				self.cursor.execute('SELECT gameDescription, gameName, thumbnailURL, gameID FROM boardgames \
+				self.cursor.execute('SELECT gameName, thumbnailURL, gameID FROM boardgames \
 				where gameID =%s', (r,))
 				resultList.append((self.cursor.fetchall())[0])
-			resultDict = self.convertDict(resultList)
+			resultDict = self.convertDictFromSome(resultList)
 			return resultDict
 		except Exception as e:
 			print('Curcor error', e)
@@ -195,6 +220,7 @@ class DataSource:
 			resultList = self.cursor.fetchall()
 			if not resultList:
 				return -1
+			#converts the list to a dictionary for all the attributes
 			entry = resultList[0]
 			resultDict = {"desc": entry[0], "URL": entry[1], "maxPlayer": entry[2], "maxTime":entry[3] 
 						, "minAge": entry[4], "minPlayer": entry[5], "minTime": entry[6], "name": entry[7]
@@ -211,98 +237,12 @@ class DataSource:
 				self.connection.close()
 				self.connection = connection = psycopg2.connect(database=self.database, user=self.user, password=self.password,host = "localhost")
 			self.cursor = self.connection.cursor()
-		#change all attributes to appropriate values 
 
-	def convertDict(self, list):
+
+	#convert the resulting lists from randomSearch, listAZSearch and newGamesSearch to a dictionary with appropriate keys
+	def convertDictFromSome(self, list):
 		dict = []
 		for i in range(len(list)):
-			dict.append({"desc": list[i][0], "name": list[i][1], "thumbURL": list[i][2], "ID": list[i][3]})
+			dict.append({"name": list[i][0], "thumbURL": list[i][1], "ID": list[i][2]})
 		return dict
 
-# def main():
-# 	newQuery = DataSource()
-# # 	print("Results for new games search. \n")
-
-# 	returnDict = newQuery.returnAllAttributes(23)
-# 	print(returnDict)
-
-# 	a = newQuery.randomSearch()
-# 	print(a)
-
-# 	b = newQuery.newGamesSearch()
-# 	print(b)
-	# #Test mainSearch()
-	# list1 = newQuery.mainSearch(-1, -1, "war")
-	# list2 = newQuery.mainSearch(-1, 4, "war")
-	# list3 = newQuery.mainSearch(-1, 0, "")
-	# list4 = newQuery.mainSearch("good", 3, "hfjrwhvijefhvweehgggggivivfbi")
-
-	# print()
-	# print("Results for main search list1: \n")
-	# for i in range(3):
-	# 	print(list1[i][7] + " ")
-
-
-	# print()
-	# print("Results for main search list2: \n")
-	# for i in range(3):
-	# 	print(list2[i][7] + " ")
-
-	# print()
-	# print("Results for main search list3: \n")
-	# for i in range(3):
-	# 	print(list3[i][7] + " ")
-
-	# print()
-	# print("Results for main search list4: \n")
-	# print(list4) #should return -1
-
-	# print()
-	# print()
-
-	# #Test listAZSearch
-	# print("Results for games A-Z search. \n")
-	# list5 = newQuery.listAZSearch()
-	# for i in range(20):
-	# 	print(list5[i][1])
-	# 	print()
-
-	# print()
-	# print()
-
-	# #Test newGamesSearch
-	# print("Results for new games search. \n")
-	# list6 = newQuery.newGamesSearch()
-	# for i in range(20):
-	# 	print(list6[i][1])
-	# 	print()
-
-	# print()
-	# print()
-
-	# #Test randomSearch
-	# print("Results for random games search. \n")
-	# list7 = newQuery.randomSearch()
-	# for i in range(7):
-	# 	print(list7[i][1])
-	# 	print()
-
-	# print()
-	# print()
-
-	# #Test returnAllAttributes
-	# print("Results for a particular game. \n")
-	# for i in range(7):
-	# 	gameAttr = newQuery.returnAllAttributes(list7[i][3])
-	# 	print("Attributes for game", (i + 1))
-	# 	for j in range(17):
-	# 		print(gameAttr[j])
-	# 		print()
-	# 	print()
-#main()
-
-
-
-
-
-    
--- gamesFlask.py
+++ gamesFlask.py
@@ -1,64 +1,60 @@
 #!/usr/bin/env python3
 '''
     James Yang, Vermillion Villarreal
-    Last updated: October 23rd 
+    Last updated: October 25th
 '''
 from datasource import DataSource
 import flask
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 import json
 import sys
 
+#global variables
 app = Flask(__name__, template_folder='templates')
 newQuery = DataSource()
 A_ZList = newQuery.listAZSearch()
 newGamesList = newQuery.newGamesSearch()
 
+#generates random games and displays on home page
 @app.route('/', methods = ['POST', 'GET'])
 def homePage():
-    if request.method == 'POST':
-        return redirect(url_for('/<genre>+<minPlayers>+<search>/'), code = 307)
     randomList = newQuery.randomSearch()
     #check if URL is valid
-    return render_template('.html', list = randomList)
+    return render_template('index.html', list = randomList)
 
-#default to 10 results per page for now
+'''
+displays the first 99 games of all the games from A to Z 
+because displaying everything at once takes too long to load
+'''
 @app.route('/a_z/', methods = ['POST', 'GET'])
 def azPage():
-    if request.method == 'POST':
-        return redirect(url_for('/<genre>+<minPlayers>+<search>/'), code = 307)
     return render_template('results.html', list = A_ZList, total = len(A_ZList))
 
+#displays 99 new games 
 @app.route('/newgames/', methods = ['POST', 'GET'])
 def newgamesPage():
-    if request.method == 'POST':
-        return redirect(url_for('/<genre>+<minPlayers>+<search>/'), code = 307)
     return render_template('results.html', list = newGamesList, total = len(newGamesList))
 
-@app.route('/<genre>+<minPlayers>+<search>/', methods = ['POST'])
-def searchPage_form(genre, minPlayers, search):
-    genre = request.form.get['genre']
-    minPlayers = request.form.get['minPlayers']
-    search = request.form.get['search']
-    if genre == 'NA':
-        genre = -1
-    if minPlayers == 'NA':
-        minPlayers = -1
-    if minPlayers == 'NA':
-        minPlayers = ''
-    return redirect(url_for('/<genre>+<minPlayers>+<search>/', 
-                            genre = genre, minPlayers = minPlayers, search = search),
-                            code = 302)
+#gathers input from html form and inputs for search in datasource.py
+@app.route('/search/', methods = ['POST', 'GET'])
+def searchPage_form():
+    if request.method == 'POST':
+        genre = request.form.get('genre')
+        minPlayers = request.form.get('minPlayers')
+        search = request.form.get('search')
+        if genre == '':
+            genre = -1
+        resultList = newQuery.mainSearch(genre, minPlayers, search)             
+        if resultList != -1 or len(resultList) == 0:
+            return render_template('results.html', list = resultList, total = len(resultList))
+        return render_template('notFound.html')
 
-@app.route('/<genre>+<minPlayers>+<search>/')
-def searchPage_create(genre, minPlayers, search):
-    resultList = newQuery.mainSearch(genre, minPlayers, search);
-    if resultList:
-        return render_template('results.html', list = resultList, total = len(resultList))
-    return render_template('HTML of no result page')
-
-@app.route('/<gameID>/')
+#gets to the individual page of each game with input game ID
+@app.route('/<gameID>/', methods = ['POST', 'GET'])
 def searchByID(gameID):
+    if gameID == 'favicon.ico' or gameID == 'NA':
+        randomList = newQuery.randomSearch()
+        return render_template('index.html', list = randomList)
     resultList = newQuery.returnAllAttributes(gameID)
     return render_template('specific.html', list = resultList)
 
@@ -66,7 +62,6 @@ if __name__ == '__main__':
     if len(sys.argv) != 3:
         print('Usage: {0} host port'.format(sys.argv[0]), file=sys.stderr)
         exit()
-
     host = sys.argv[1]
     port = sys.argv[2]
     app.run(host=host, port=port)
